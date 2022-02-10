e927947b1c9f82c295b77ae3ad2de85a1a2ac851
angelitoconceptmachine <angelito@concept-machine.net>
2022-02-10 22:55:27 +0800

    wip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 const routes: Routes = [
   {path:'login', component:LoginComponent},
+  { path: 'pages',   redirectTo: '/pages/billing', pathMatch: 'full' },
   { path: 'pages', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
+  
 ];
 
 @NgModule({
--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PagesComponent } from './pages.component';
 
-const routes: Routes = [{ path: '', component: PagesComponent }];
+const routes: Routes = [
+  { path: '', component: PagesComponent, children:[
+  {
+    path: 'billing',
+    loadChildren: () => import('./billing/billing.module')
+      .then(m => m.BillingModule),
+  },
+] }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
